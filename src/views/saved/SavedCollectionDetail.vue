03c2f7733f0c21f7b6c3a538a8e30d8116419d3c
Tường Nhâm Bổn <77843790+Tuong1994@users.noreply.github.com>
2024-12-23 18:50:00 +0000

    Collection Detail (30%)

--- src/views/saved/SavedCollectionDetail.vue
+++ src/views/saved/SavedCollectionDetail.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { Card, Image, Section, Grid, Typography } from '@/components/UI'
+import { Card, Image, Space, Section, Button, Icon, Grid, Typography, Dropdown } from '@/components/UI'
+import { iconName } from '@/components/UI/Icon/constant'
+import ItemWrapper from '@/components/View/ItemWrapper/ItemWrapper.vue'
 import defaultImage from '/default.jpg'
 
 const { Row, Col } = Grid
@@ -21,7 +23,32 @@ const { Paragraph } = Typography
       </template>
     </Card>
     <Card rootClassName="detail-head">
-      <template #body> </template>
+      <template #body>
+        <Row justify="between" aligns="middle">
+          <Col>
+            <Paragraph :size="18" :weight="600">Collection title</Paragraph>
+          </Col>
+          <Col>
+            <Dropdown placement="right" labelClassName="!p-0">
+              <template #label>
+                <Button>
+                  <Icon :iconName="iconName.ELLIPSIS_H" />
+                </Button>
+              </template>
+              <template #dropdown>
+                <div class="p-1">
+                  <ItemWrapper>
+                    <Space aligns="middle">
+                      <Icon :iconName="iconName.TRASH" />
+                      <Paragraph>Delete collection</Paragraph>
+                    </Space>
+                  </ItemWrapper>
+                </div>
+              </template>
+            </Dropdown>
+          </Col>
+        </Row>
+      </template>
     </Card>
   </Section>
 </template>
